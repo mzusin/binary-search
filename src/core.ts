078a60027fc2652974dc5e1be0c4ab8d23826103
Miriam Zusin <miriamzusin@gmail.com>
2023-08-30 21:57:28 +0300

    iterative binary search

--- src/core.ts
+++ src/core.ts
@@ -25,16 +25,18 @@ export const binarySearchIterative = (nums: number[], target: number) : number =
     let start = 0;
     let end = nums.length - 1;
 
-    while(start < end) {
+    while(start <= end) {
         const middle = start + Math.floor((end - start)/2);
 
-        if(target <= nums[middle]) {
-            end = middle;
+        if(nums[middle] === target) return middle;
+
+        if(target < nums[middle]) {
+            end = middle - 1;
         }
         else{
             start = middle + 1;
         }
     }
 
-    return nums[start] === target ? start : -1;
+    return -1;
 };
