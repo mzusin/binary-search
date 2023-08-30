e113e19576d63f74b390537271a26038fc7f45c4
Miriam Zusin <miriamzusin@gmail.com>
2023-08-30 21:44:36 +0300

    iterative binary search

--- src/core.ts
+++ src/core.ts
@@ -14,3 +14,46 @@ export const binarySearchRecursive = (nums: number[], target: number) : number =
 
     return helper(0, n - 1);
 };
+
+export const binarySearchIterative = (nums: number[], target: number) : number => {
+
+    if(nums.length <= 0) return -1;
+
+    let start = 0;
+    let end = nums.length - 1;
+
+    while(start < end) {
+        const middle = start + Math.floor((end - start)/2);
+
+        if(target <= nums[middle]) {
+            end = middle;
+        }
+        else{
+            start = middle + 1;
+        }
+    }
+
+    return nums[start] === target ? start : -1;
+};
+
+
+/*
+def find_left(nums: List[int], target: int) -> int:
+  lo, hi = 0, len(nums) - 1
+
+  while lo < hi:  # Stop searching when lo meets hi.
+    mid = (hi - lo) // 2 + lo  # Find the midway point.
+
+    if target <= nums[mid]:  # Customize this condition.
+      # Search the left side (including mid).
+      hi = mid
+    else:
+      # Search the right side (not including mid).
+      lo = mid + 1
+
+  # Check if we found our target.
+  if nums[lo] != target:
+    return -1
+
+  return lo  # Our target should be at lo.
+ */
--- test/core.test.ts
+++ test/core.test.ts
@@ -1,4 +1,4 @@
-import { binarySearchRecursive } from '../src/core';
+import { binarySearchRecursive, binarySearchIterative } from '../src/core';
 
 describe('Core', () => {
 
@@ -30,6 +30,37 @@ describe('Core', () => {
             const nums = [1, 2, 3, 3, 4, 5, 6];
             expect(binarySearchRecursive(nums, 3)).toEqual(2);
             expect(binarySearchRecursive(nums, 6)).toEqual(6);
+        });*/
+    });
+
+    describe('binarySearchIterative()', () => {
+        it('should return the index of the target element in a sorted array', () => {
+            const nums = [1, 2, 3, 4, 5, 6, 7];
+            expect(binarySearchIterative(nums, 3)).toEqual(2);
+            expect(binarySearchIterative(nums, 6)).toEqual(5);
+        });
+
+        it('should return -1 when the target element is not found in the array', () => {
+            const nums = [1, 2, 3, 4, 5, 6, 7];
+            expect(binarySearchIterative(nums, 8)).toEqual(-1);
+            expect(binarySearchIterative(nums, 0)).toEqual(-1);
+        });
+
+        it('should handle an empty array and return -1', () => {
+            const nums: number[] = [];
+            expect(binarySearchIterative(nums, 3)).toEqual(-1);
+        });
+
+        it('should handle a single-element array', () => {
+            const nums = [1];
+            expect(binarySearchIterative(nums, 1)).toEqual(0);
+            expect(binarySearchIterative(nums, 2)).toEqual(-1);
+        });
+
+        /*it('should handle an array with duplicate elements', () => {
+            const nums = [1, 2, 3, 3, 4, 5, 6];
+            expect(binarySearchIterative(nums, 3)).toEqual(2);
+            expect(binarySearchIterative(nums, 6)).toEqual(6);
         });*/
     });
 });
