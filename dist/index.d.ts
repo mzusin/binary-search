0915f4584ec38a5e4fd890512ece3bd4899052eb
Miriam Zusin <miriamzusin@gmail.com>
2023-09-14 11:39:35 +0300

    update types file

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,11 @@
-declare module 'mz-binary-search' {
+module 'mz-binary-search' {
+    export const getMiddle: (left: number, right: number) => number;
     export const binarySearchRecursive: (nums: number[], target: number) => number;
+    export const binarySearchRecursiveLeftMost: (nums: number[], target: number) => number;
+    export const binarySearchRecursiveRightMost: (nums: number[], target: number) => number;
+    export const binarySearchIterative: (nums: number[], target: number) => number;
+    export const binarySearchIterativeLeftMost: (nums: number[], target: number) => number;
+    export const binarySearchIterativeRightMost: (nums: number[], target: number) => number;
+    export const findFirstNonNegativeNumberIterative: (nums: number[]) => number;
+    export const countNegativeNumbersIterative: (nums: number[]) => number;
 }
